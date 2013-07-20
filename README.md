31e96fc18ca63bcfc191c1a3f335a34907686c5f
Zohar Arad <zohar@zohararad.com>
2013-07-20 18:06:33 +0300

    add support for basic response caching

--- README.md
+++ README.md
@@ -31,12 +31,57 @@ module.exports.adapters = {
     port     : 80,                 // api port
     protocol : 'http',             // HTTP protocol (http | https)
     pathname : '/api/v1'           // base api path
+    cache    : {
+      engine : require('someCacheEngine') // see [Caching](#Caching)
+    }
   }
 
 };
 ```
 
+## Caching
+
+To cache API responses, you can add an object to the adapter's configuration with an attribute named `engine` that responds
+to the following methods:
+
+* `get(key)` - get cache key
+* `set(key, value)` - set value on key
+* `del(key)` - delete value identified by `key` from cache
+
+An example caching configuration using LRU cache (`npm install --save lru-cache`) will look something like:
+
+```javascript
+// under config/cache.js
+
+var LRU = require("lru-cache"),
+    options = {
+      max: 500,
+      maxAge: 1000 * 10
+    },
+    cache = LRU(options);
+module.exports = cache;
+
+// under config/adapters.js
+
+  rest: {
+    module   : 'sails-rest',
+    hostname : 'api.somewhere.io', // api hostname
+    port     : 80,                 // api port
+    protocol : 'http',             // HTTP protocol (http | https)
+    pathname : '/api/v1'           // base api path
+    cache    : {
+      engine : require('./cache')
+    }
+  }
+```
+
+Cache keys are computed from the API request URLs. This means that each unique URL will have its own cache key.
+
+At the moment, cache busting is done by exact key only. This means that if you fetch `/users` and then update
+`/users/1`, the cached objects under `/users` will not be purged to reflect your changes.
+
 ## TODO
 
-* Add some sort of caching support
+* Improve cache busting
+* Add support for async cache engines (eg. redis_client)
 * Some create tests currently fail
--- SailsRest.js
+++ SailsRest.js
@@ -12,7 +12,8 @@ module.exports = (function(){
   "use strict";
 
   var connection = {},
-      defs = {};
+      defs = {},
+      cache;
 
   // Private functions
   /**
@@ -49,7 +50,10 @@ module.exports = (function(){
    */
   function createConnection(collection){
     defs[collection.identity] = collection.definition;
-    var c = _.extend({}, collection.defaults,collection.config);
+    if(collection.config.cache){
+      cache = collection.config.cache;
+    }
+    var c = _.extend({}, collection.defaults, collection.config);
     return {
       protocol: c.protocol,
       hostname: c.host,
@@ -122,6 +126,9 @@ module.exports = (function(){
     create: function(collectionName, values, cb) {
       var uri = getUrl(collectionName);
       rest.post(uri, {data: values}).on('success', function(data, response){
+        if(cache){
+          cache.engine.del(uri);
+        }
         cb(null, formatResult(data, collectionName));
       }).on('error', function(err, response){
         cb(err, response);
@@ -130,16 +137,28 @@ module.exports = (function(){
 
     find: function(collectionName, options, cb){
       var uri = getUrl(collectionName, options);
-      rest.get(uri).on('success', function(data, response){
-        cb(null, getResultsAsCollection(data, collectionName));
-      }).on('error', function(err, response){
-        cb(err, response);
-      });
+      var r = cache && cache.engine.get(uri);
+      if(r){
+        cb(null, r);
+      } else {
+        rest.get(uri).on('success', function(data, response){
+          var r = getResultsAsCollection(data, collectionName);
+          if(cache){
+            cache.engine.set(uri, r);
+          }
+          cb(null, r);
+        }).on('error', function(err, response){
+          cb(err, response);
+        });
+      }
     },
 
     update: function(collectionName, options, values, cb) {
       var uri = getUrl(collectionName, options);
       rest.put(uri, {data: values}).on('success', function(data, response){
+        if(cache){
+          cache.engine.del(uri);
+        }
         cb(null, getResultsAsCollection(data, collectionName));
       }).on('error', function(err, response){
         cb(err, response);
